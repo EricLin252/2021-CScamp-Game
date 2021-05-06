c08631307c049544ffc4e0a0d035caccf89d3ed7
EricLin252 <jamielin61207@gmail.com>
2021-05-06 17:48:14 +0800

    feat: 子彈射擊

--- js/component.js
+++ js/component.js
@@ -1,14 +1,17 @@
 //background設定
 Crafty.c("BG", {
+	//初始化，並綁定滑鼠點擊事件
 	init: function(){
 		this.addComponent("Canvas, Mouse")
 			.bind("Click", this.mapClick);
 	},
+	//設定地圖圖片以及長寬
 	setImg: function(url, w, h){
 		Crafty.sprite(url, {BGimg: [0, 0, w, h]});
 		this.addComponent("BGimg");
 		return this;
 	},
+	//滑鼠點擊事件發生時會運作
 	mapClick: function(data){
 		var playerPos = Crafty("Player").getCenter();
 		var mousePos = {
@@ -16,11 +19,36 @@ Crafty.c("BG", {
 			y: data.realY
 		};
 		var playerRadius = Crafty("Player").w/2;
-		Crafty("Player").weapon.setPos(playerPos, mousePos, playerRadius);
+		Crafty("Player").weapon.fire(playerPos, mousePos, playerRadius);
 	}
 });
 
-var weaponErr = {x: 2.5, y: 2, r: 5};
+Crafty.c("Bullet", {
+	init: function(){
+		this.addComponent("2D, DOM, Color")
+			.attr({w: 10, h: 15})
+			.color("gray")
+			.origin("center")
+			.bind("UpdateFrame", this.fly);
+
+		this.v = 300;
+		this.lifetime = 3;
+		this.timer = 0;
+	},
+	fly: function(data){
+		this.timer += data.dt / 1000;
+		if(this.timer > this.lifetime) this.destroy();
+
+		this.attr({
+			x: this.x + this.direction.x * this.v * data.dt/1000,
+			y: this.y + this.direction.y * this.v * data.dt/1000
+		});
+	}
+});
+
+//因player的DOM元素顯示存在些許誤差，需要微調武器的顯示位置(單位：px)
+//x: 橫向偏移 / y: 縱向偏移 / r: player與武器之間預留空間 / b: 子彈生成與武器之間的預留空間
+var weaponErr = {x: 2.5, y: 2, r: 5, b: 2};
 
 //weapon基本屬性設定
 Crafty.c("Weapon", {
@@ -30,7 +58,9 @@ Crafty.c("Weapon", {
 			.origin("center")
 			.color("red");
 	},
-	setPos: function(playerPos, mousePos, playerRadius){
+	//由滑鼠點擊事件呼叫
+	fire: function(playerPos, mousePos, playerRadius){
+		//更改武器的位置與朝向
 		playerRadius += this.h/2 + weaponErr.r;
 		var distance = Crafty.math.distance(playerPos.x, playerPos.y, mousePos.x, mousePos.y);
 		var Vec = new Crafty.math.Vector2D(mousePos.x - playerPos.x, mousePos.y - playerPos.y);
@@ -38,12 +68,31 @@ Crafty.c("Weapon", {
 			x: playerPos.x * (distance-playerRadius)/distance + mousePos.x * playerRadius/distance,
 			y: playerPos.y * (distance-playerRadius)/distance + mousePos.y * playerRadius/distance
 		};
-		this.attr(this.center_to_topleft(newCenter));
+		var newTopleft = this.center_to_topleft(newCenter);
 		var Rad = Vec.angleBetween(new Crafty.math.Vector2D(0, 1));
-		this.rotation = -Crafty.math.radToDeg(Rad);
+		this.attr({
+			x: newTopleft.x + weaponErr.x,
+			y: newTopleft.y + weaponErr.y,
+			rotation: -Crafty.math.radToDeg(Rad)
+		});
+
+		//生成子彈
+		var bullet = Crafty.e("Bullet");
+		var fireDiff = Vec.normalize().scale(this.h/2 + bullet.h/2 + weaponErr.b);
+		bullet.attr({
+			x: newCenter.x - bullet.w/2 + weaponErr.x + fireDiff.x,
+			y: newCenter.y - bullet.h/2 + weaponErr.y + fireDiff.y,
+			rotation: this.rotation
+		});
+		bullet.direction = Vec.normalize();
 	},
+	//將武器中央座標轉換為左上角座標
 	center_to_topleft: function(center){
-		return {x: center.x - this.w/2 + weaponErr.x, y: center.y - this.h/2 + weaponErr.y};
+		return {x: center.x - this.w/2, y: center.y - this.h/2};
+	},
+	//將武器左上角座標轉換為中央座標
+	topleft_to_center: function(topleft){
+		return {x: topleft.x + this.w/2, y: topleft.y + this.h/2};
 	}
 });
 
