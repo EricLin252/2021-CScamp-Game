bbab68e7f4db32561df9bfc618bee6569b22dff1
EricLin252 <jamielin61207@gmail.com>
2021-05-10 19:08:45 +0800

    重構: 將各component常用的共同function拉出來

---
+++ js/tool.js
@@ -0,0 +1,17 @@
+function center_to_topleft(center, size){
+    return {x: center.x - size.w/2, y: center.y - size.h/2};
+}
+
+function topleft_to_center(topleft, size){
+    return {x: topleft.x + size.w/2, y: topleft.y + size.h/2};
+}
+
+function get_landscape(position){
+    var x = Math.round(position.x);
+    var y = Math.round(position.y);
+    var count = 0;
+    for(var i = 0; i < landscape[y].length; i++){
+        count += landscape[y][i][1];
+        if(count >= x) return landscape[y][i][0];
+    }
+}
